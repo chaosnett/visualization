b7db5901626e4eab53981bc0fe8ff0f8961a7d72
neelthepatel <patel.neel5@northeastern.edu>
2024-10-13 05:04:45 -0400

    remove instructions

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
-"use client";
 /* eslint-disable */
 
 import { Header } from "@/components/Header";
 import Graph from "@/components/Graph";
-import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [showInstructions, setShowInstructions] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowInstructions(false);
-    }, 5000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="relative h-screen bg-secondary font-array tracking-wider text-primary">
       <Header className="absolute right-10 top-10 z-10" title="chaosnet." />
@@ -31,15 +19,6 @@ export default function Home() {
       >
         Check out our DevLogs!
       </a>
-
-      <div
-        className={`absolute bottom-10 right-10 flex flex-col items-start justify-center gap-1.5 rounded-xl border-2 border-black bg-white px-4 py-2 shadow-2xl transition-opacity duration-[2s] ${showInstructions ? "opacity-100" : "pointer-events-none opacity-0"} `}
-      >
-        <span className="pl-2 text-lg font-bold">Try it out:</span>
-        <span>- Move nodes around</span>
-        <span>- Click a Country Node to See Info</span>
-        <span>- Start a new simulation</span>
-      </div>
     </div>
   );
 }
